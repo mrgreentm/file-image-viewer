77be2984fc6f119fdebf9dfb68e77839ef48755b
joao jacinto <joaonetotfm@gmail.com>
2023-06-11 17:18:52 -0300

    feature: redução de código

--- projects/file-viewer/src/lib/modules/file-viewer/file-viewer.component.ts
+++ projects/file-viewer/src/lib/modules/file-viewer/file-viewer.component.ts
@@ -1,17 +1,16 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'lib-file-viewer',
   templateUrl: './file-viewer.component.html',
   styleUrls: ['./file-viewer.component.scss'],
 })
-export class FileViewerComponent implements OnInit {
-  @Input() imageUrl!:string;
-  @Output('download') downloadUrl= new EventEmitter<string>();
+export class FileViewerComponent {
+  @Input() imageUrl!: string;
+  @Output('download') downloadUrl = new EventEmitter<string>();
   width = 80;
-  constructor() {}
 
-  ngOnInit(): void {}
-
-
+  zoom(event: number) {
+    this.width = event;
+  }
 }
