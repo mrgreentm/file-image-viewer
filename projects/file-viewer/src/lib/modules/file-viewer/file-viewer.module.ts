b92774d3561a70d515cb1d590d2adaaedb287e64
joao jacinto <joaonetotfm@gmail.com>
2023-06-12 22:13:56 -0300

    feature: muda mat icon para svg e remove dependencias desnecessárias

--- projects/file-viewer/src/lib/modules/file-viewer/file-viewer.module.ts
+++ projects/file-viewer/src/lib/modules/file-viewer/file-viewer.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { FileViewerComponent } from './file-viewer.component';
 import { CommonModule } from '@angular/common';
 
-import { MatIconModule } from '@angular/material/icon';
 import { ActionBarComponent } from '../action-bar/action-bar/action-bar.component';
 
 
 @NgModule({
   declarations: [FileViewerComponent, ActionBarComponent],
-  imports: [MatIconModule, CommonModule],
+  imports: [CommonModule],
   exports: [FileViewerComponent, ActionBarComponent],
 })
 export class FileViewerModule {}
